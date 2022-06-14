3fff18b2b458600ae8423b3389d7455df6561a24
HyeonUk kim <kimdo20@naver.com>
2022-06-14 21:16:23 +0900

    Create nodemon 설치, GET,POST 순수 JS

--- src/main.js
+++ src/main.js
@@ -10,8 +10,20 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.end('Hello')
+  console.log(req.url)
+  if (req.url === '/posts' && req.method === 'GET') {
+    res.statusCode = 200
+    res.end('List of Posts')
+  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
+    res.statusCode = 200
+    res.end('Some content of the post')
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    res.statusCode = 200
+    res.end('Creating post')
+  } else {
+    res.statusCode = 404
+    res.end('Not found')
+  }
 })
 
 const PORT = 4000
