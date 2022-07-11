bbfae5d02012bf3a7edb315fb4675d97d0fd4fa3
HyeonUk kim <kimdo20@naver.com>
2022-07-11 21:49:45 +0900

    node 핵심 개념정리, require

--- src/main.js
+++ src/main.js
@@ -1,62 +1,13 @@
 // @ts-check
-// 프레임 워크 없이 웹 서버 ㄱㄱ
 
-/**
- *   블로그 포스팅 서비스
- *   - 로컬 파일을 데이터베이스로 활용
- *   - 인증 로직은 넣지 않는다.
- *   - RESTful API 사용
- */
-const http = require('http')
+// require
 
-/**
- * @typedef Post
- * @property {string} id
- * @property {string} title
- * @property {string} content
- */
+const { path, paths, filename } = module
 
-/** @type {Post[]} */
-const posts = [
-  {
-    id: 'my_first_post',
-    title: 'My first post',
-    content: 'Hello!',
-  },
-  {
-    id: 'my_second_post',
-    title: 'My second post',
-    content: 'muyaho',
-  },
-]
-console.log(posts)
-
-const server = http.createServer((req, res) => {
-  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_])+$/
-  const postIdRegexResult =
-    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
-
-  if (req.url === '/posts' && req.method === 'GET') {
-    res.statusCode = 200
-    res.end('List of Posts')
-  } else if (postIdRegexResult) {
-    // console.log(postIdRegexResult)
-    if (postIdRegexResult) {
-      const postId = postIdRegexResult[1]
-      console.log(`postId: ${postId}`)
-    }
-    res.statusCode = 200
-    res.end('Reading a post')
-  } else if (req.url === '/posts' && req.method === 'POST') {
-    res.statusCode = 200
-    res.end('Creating post')
-  } else {
-    res.statusCode = 404
-    res.end('Not found')
-  }
+console.log({
+  path,
+  paths,
+  filename,
 })
 
-const PORT = 4000
-server.listen(PORT, () => {
-  console.log(`The server is listening at port: ${PORT}`)
-})
+console.log(require('./animals'))
